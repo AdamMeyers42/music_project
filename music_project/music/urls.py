06c6a11ccaffd440f0cee3ff160515f0f9df7b8b
AdamMeyers42 <adammeyers3551@gmail.com>
2021-11-02 06:57:53 -0700

    Completed final edits on on views and urls, having two classes

--- music_project/music/urls.py
+++ music_project/music/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('music/', views.SongList.as_view()),
-    path('music/<int:id>/', views.SongList.get_single),
+    path('music/<int:id>/', views.SongDetail.as_view()),
 ]
+
+    # path('music/<int:id>/', views.SongDetail.put),
+    # path('music/<int:id>/', views.SongDetail.get),
+    # path('music/<int:id>/', views.SongDetail.delete)
--- music_project/music/views.py
+++ music_project/music/views.py
@@ -6,8 +6,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
 
 # Create your views here.
 class SongList(APIView):
@@ -24,8 +22,33 @@ class SongList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    @api_view(('GET',))
-    def get_single(self, id):
-        song = Song.objects.get(id=id)
-        serializer = SongSerializer(song, many=False)
+class SongDetail(APIView):
+    
+    # def get_single(self, id):
+    #     song = Song.objects.get_object(id)
+    #     serializer = SongSerializer(song)
+    #     return Response(serializer.data)        
+    def get_object(self, id):
+        try:
+            return Song.objects.get(id=id)
+        except: Song.DoesNotExist 
+        raise Http404
+    
+    def get(self, request, id):
+        song = self.get_object(id)
+        serializer = SongSerializer(song)
         return Response(serializer.data)
+    
+    def put(self, request, id):
+        song = self.get_object(id)
+        serializer = SongSerializer(song, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        song = self.get_object(id)
+        serializer = SongSerializer(song)
+        song.delete()
+        return Response(serializer.data, status=status.HTTP_200_OK)
